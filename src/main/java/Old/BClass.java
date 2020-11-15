da6e5026b0e2573f5c78677f2375e323c2f48d86
ViktoriaB <nva0045@gmail.com>
2020-11-15 20:27:53 +0200

    second part

---
+++ src/main/java/Old/BClass.java
@@ -0,0 +1,35 @@
+package Old;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BClass extends CClass {
+
+
+    private static final Logger LOG = LogManager.getLogger("Класс В ");
+    String name;
+    int number;
+
+    public  BClass () {
+        LOG.info("Новый пустой экземпляр класса " + getClass());
+        LOG.info("=========");
+
+    }
+    public BClass (String name, int number) {
+        this.name = name;
+        this.number = number;
+
+    }
+//создаем метод для сортировки данных:
+    public void sortColors(List<String> list) {
+//сортируем список:
+        Collections.sort(list);
+//создаем цикл и выводим список:
+        for (int i = 0; i < list.size(); i++) {
+            LOG.debug(list.get(i));
+        }
+    }
+}
